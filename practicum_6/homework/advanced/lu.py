538ea2368c6d2660b3b17bdfaec02049c1906833
whytimmy <65584034+whytimmy@users.noreply.github.com>
2024-05-04 03:20:53 +0300

    Update lu.py

--- practicum_6/homework/advanced/lu.py
+++ practicum_6/homework/advanced/lu.py
@@ -1,8 +1,9 @@
 from collections import defaultdict
 from dataclasses import dataclass
 import os
-import yaml
 import time
+import yaml
+
 
 import numpy as np
 from numpy.typing import NDArray
@@ -11,31 +12,44 @@ import scipy.linalg
 
 from src.linalg import get_scipy_solution
 
-
 @dataclass
 class Performance:
-    time: float = 0.0
+    time1: float = 0.0
     relative_error: float = 0.0
-
-
 def lu(A: NDArray, permute: bool) -> tuple[NDArray, NDArray, NDArray]:
 
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
+    n = len(A)
+    U = np.copy(A)
+    L = np.eye(A)
+    P = np.eye(A)
 
-    pass
+    for k in range(n - 1):
+        if permute:
+            max_index = np.argmax(abs(U[k:, k])) + k
+            if max_index != k:
+                U[[k, max_index]] = U[[max_index, k]]
+                P[[k, max_index]] = P[[max_index, k]]
+                if k != 0:
+                    L[[k, max_index], :k] = L[[max_index, k], :k]
 
+        for s in range(k + 1, n):
+            L[s][k] = U[s][k] / U[k, k]
+            U[s] -= U[k] * U[s][k] / U[k, k]
 
+    return L, U, P
 def solve(L: NDArray, U: NDArray, P: NDArray, b: NDArray) -> NDArray:
 
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
+    n = U.shape[0]
+    x, y = np.zeros(n), np.zeros(n)
+    b_rearranged = P.dot(b)
 
-    pass
+    for i in range(n):
+        y[i] = b_rearranged[i] - L[i, :i].dot(y[:i])
 
+    for i in range(n-1, -1, -1):
+        x[i] = (y[i] - U[i, i+1:].dot(x[i+1:])) / U[i, i]
 
+    return x
 def run_test_cases(n_runs: int, path_to_homework: str) -> dict[str, Performance]:
     matrix_filenames = []
     performance_by_matrix = defaultdict(Performance)
@@ -54,7 +68,7 @@ def run_test_cases(n_runs: int, path_to_homework: str) -> dict[str, Performance]
             t1 = time.time()
             L, U, P = lu(A, permute=True)
             t2 = time.time()
-            perf.time += t2 - t1
+            perf.time1 += t2 - t1
             if j == 0:  # first run => compute solution
                 x = solve(L, U, P, b)
                 x_exact = get_scipy_solution(A, b)
