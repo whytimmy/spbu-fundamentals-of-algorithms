9d421fe7e0af27eaa103a518087d4de74e7f8343
whytimmy <65584034+whytimmy@users.noreply.github.com>
2024-05-18 14:01:21 +0300

    Update simulated_annealing.py

--- practicum_5/homework/advanced/simulated_annealing.py
+++ practicum_5/homework/advanced/simulated_annealing.py
@@ -48,6 +48,8 @@ def solve_via_simulated_annealing(
     MAX_T = 500
     rate = 0.95
     current_t = MAX_T
+    counter = 0
+    loss_history = np.zeros((n_iters,), dtype=np.int_)
 
     for _ in range(n_iters):
         new_colors = tweak(G, current_colors, n_max_colors)
@@ -57,23 +59,29 @@ def solve_via_simulated_annealing(
         if transition_probability(delta, current_t) >= np.random.random():
             current_colors = new_colors.copy()
             current_conflicts = new_conflicts
+            loss_history[counter] = current_conflicts
+            counter += 1
+
         current_t *= rate
-    return current_conflicts
+    return loss_history, current_conflicts
 
 
 if __name__ == "__main__":
     seed = 42
     np.random.seed(seed)
     G = nx.erdos_renyi_graph(n=100, p=0.05, seed=seed)
-    #plot_graph(G)
+    plot_graph(G)
 
     n_max_iters = 500
     n_max_colors = 3
     initial_colors = np.random.randint(low=0, high=n_max_colors - 1, size=len(G.nodes))
 
-    conflicts = solve_via_simulated_annealing(
+    loss_history, ans = solve_via_simulated_annealing(
         G, n_max_colors, initial_colors, n_max_iters
     )
-    print(f'Количество конфликтов: {conflicts}')
+    #print(ans) - количество конфликтов = 9
+    plot_loss_history(loss_history)
+
+
 
 
